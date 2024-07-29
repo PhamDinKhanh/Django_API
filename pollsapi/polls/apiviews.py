ddab76d758ec0a9d626ef2929799a16abc6f7bb3
DESKTOP-APATF5C\phamd <phamdinhkhanh.tc@gmail.com>
2024-07-29 23:40:03 +0700

    Commit code implement to step More views and viewsets

---
+++ pollsapi/polls/apiviews.py
@@ -0,0 +1,34 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+from .models import Poll, Choice
+from .serializers import PollSerializer
+from rest_framework import generics
+
+
+# class PollList(APIView):
+#     @staticmethod
+#     def get(request):
+#         polls = Poll.objects.all()[:20]
+#         data = PollSerializer(polls, many=True).data
+#         return Response(data)
+#
+#
+# class PollDetail(APIView):
+#     @staticmethod
+#     def get(request, pk):
+#         poll = get_object_or_404(Poll, pk=pk)
+#         data = PollSerializer(poll).data
+#         return Response(data)
+
+
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from .views import polls_list, polls_detail
+
+# from .apiviews import PollList, PollDetail
+#from .apiviews_generics import PollList, PollDetail, ChoiceList, CreateVote
+#from .views import polls_list, polls_detail
+from .api_viewset import PollList, PollDetail, ChoiceList, CreateVote
+
 
 urlpatterns = [
-    path("polls/", polls_list, name="polls_list"),
-    path("polls/<int:pk>/", polls_detail, name="polls_detail")
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    # path("choices/", ChoiceList.as_view(), name="choice_list"),
+    # path("vote/", CreateVote.as_view(), name="create_vote"),
+    path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote")
 ]
